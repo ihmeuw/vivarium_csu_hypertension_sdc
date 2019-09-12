2074679e5f2836049a4656478ef76f394afcd975
Kate Wilson <kwilson7@uw.edu>
2019-09-12 12:45:05 -0700

    no need for frozen set anymore

--- src/vivarium_csu_hypertension_sdc/components/globals.py
+++ src/vivarium_csu_hypertension_sdc/components/globals.py
@@ -5,7 +5,7 @@ HYPERTENSION_DOSAGES = [0.5, 1.0, 2.0]
 
 HYPERTENSIVE_CONTROLLED_THRESHOLD = 140
 
-ILLEGAL_DRUG_COMBINATION = frozenset(['ace_inhibitors', 'angiotensin_ii_blockers'])
+ILLEGAL_DRUG_COMBINATION = {'ace_inhibitors', 'angiotensin_ii_blockers'}
 
 SINGLE_PILL_COLUMNS = [f'{d}_in_single_pill' for d in HYPERTENSION_DRUGS]
 
