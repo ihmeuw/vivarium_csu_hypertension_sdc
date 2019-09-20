c13403c638d42062441b98c07b9a4be2277ad6de
James Collins <collijk@uw.edu>
2019-09-20 16:23:25 -0700

    whoops

--- src/vivarium_csu_hypertension_sdc/components/__init__.py
+++ src/vivarium_csu_hypertension_sdc/components/__init__.py
@@ -2,6 +2,5 @@ from .disease import IschemicHeartDisease, Stroke, ChronicKidneyDisease
 from .hypertension_medication import BaselineCoverage, TreatmentEffect
 from .hypertension_intervention import TreatmentAlgorithm
 from .metrics import SimulantTrajectoryObserver, MedicationObserver
-from .metrics import SimulantTrajectoryObserver, MedicationObserver
 from .risk import BetterRiskEffect
 from .testing import DummyContinuousRisk, DummyAdherence
