c4fb74c96dc18357742cad977c3051985f3a0e92
James Collins <collijk@uw.edu>
2019-11-18 20:33:30 -0800

    Remove internal dependencies

--- setup.py
+++ setup.py
@@ -17,10 +17,12 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        'vivarium==0.8.24',
-        'vivarium_public_health==0.9.18',
-        'vivarium_cluster_tools==1.0.14',
-        'vivarium_inputs[data]==3.0.1',
+        'vivarium==0.9.1',
+        'vivarium_public_health==0.10.0',
+
+        # For internal ihme use.
+        # 'vivarium_cluster_tools==1.0.14',
+        # 'vivarium_inputs[data]==3.0.1',
 
         # These are pinned for internal dependencies on IHME libraries
         'numpy<=1.15.4',
@@ -58,12 +60,17 @@ if __name__ == "__main__":
 
         install_requires=install_requirements,
 
+        # Includes data generating helpers for internal IHME use.
+        # entry_points='''
+        #        [console_scripts]
+        #        pcalculate_ckd_rr=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_ckd_rr
+        #        pcalculate_proportion_hypertensive=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_proportion_hypertensive
+        #        build_htn_artifact=vivarium_csu_hypertension_sdc.cli:build_htn_artifact
+        #        make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
+        # ''',
         entry_points='''
-               [console_scripts]
-               pcalculate_ckd_rr=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_ckd_rr
-               pcalculate_proportion_hypertensive=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_proportion_hypertensive
-               build_htn_artifact=vivarium_csu_hypertension_sdc.cli:build_htn_artifact
-               make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
+                 [console_scripts]
+                 make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
         ''',
 
         zip_safe=False,
