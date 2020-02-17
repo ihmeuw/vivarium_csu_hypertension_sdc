23da5de76b2d87fc1e396a28cb023dda60f2a3d1
James Collins <collijk@uw.edu>
2020-02-16 16:49:07 -0800

    Add extras to setup.py

--- setup.py
+++ setup.py
@@ -21,10 +21,6 @@ if __name__ == "__main__":
         'vivarium_public_health==0.10.0',
         'gbd_mapping==2.1.0',
 
-        # For internal ihme use.
-        # 'vivarium_cluster_tools==1.0.14',
-        # 'vivarium_inputs[data]==3.0.1',
-
         # These are pinned for internal dependencies on IHME libraries
         'numpy<=1.15.4',
         'tables<=3.4.0',
@@ -41,6 +37,12 @@ if __name__ == "__main__":
         'click',
         'loguru',
         'jinja2',
+    ]
+
+    extras_require = [
+        # For internal ihme use.
+        'vivarium_cluster_tools==1.1.2',
+        'vivarium_inputs[data]==3.1.1',
     ]
 
     setup(
@@ -60,19 +62,22 @@ if __name__ == "__main__":
         include_package_data=True,
 
         install_requires=install_requirements,
+        extras_require={
+            'dev': extras_require
+        },
 
         # Includes data generating helpers for internal IHME use.
-        # entry_points='''
-        #        [console_scripts]
-        #        pcalculate_ckd_rr=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_ckd_rr
-        #        pcalculate_proportion_hypertensive=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_proportion_hypertensive
-        #        build_htn_artifact=vivarium_csu_hypertension_sdc.cli:build_htn_artifact
-        #        make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
-        # ''',
         entry_points='''
-                 [console_scripts]
-                 make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
+               [console_scripts]
+               pcalculate_ckd_rr=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_ckd_rr [dev]
+               pcalculate_proportion_hypertensive=vivarium_csu_hypertension_sdc.external_data.cli:pcalculate_proportion_hypertensive [dev]
+               build_htn_artifact=vivarium_csu_hypertension_sdc.cli:build_htn_artifact [dev]
+               make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
         ''',
+        # entry_points='''
+        #          [console_scripts]
+        #          make_specs=vivarium_csu_hypertension_sdc.cli:make_specs
+        # ''',
 
         zip_safe=False,
     )
